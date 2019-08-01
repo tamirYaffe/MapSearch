e45f2774533a534cd2ae306138440c43f90e6507
tamiryaffe <tamiryaffe144@gmail.com>
2019-08-01 11:05:05 +0300

    add pathsGraph

--- src/Search/DistanceService.java
+++ src/Search/DistanceService.java
@@ -11,7 +11,8 @@ import java.util.Set;
 
 public class DistanceService {
     static DijkstraShortestPath dijkstraShortestPath;
-    private static HashMap<Position,HashMap<Position,Double>> distances=new HashMap<>();
+    static Graph<Position, UndirectedWeightedEdge> pathsGraph = new DefaultUndirectedWeightedGraph<>(UndirectedWeightedEdge.class);
+
 
     public static double minDistance(HashSet<Position> positions, Position currPosition) {
         Position distantPosition = null;
@@ -23,6 +24,7 @@ public class DistanceService {
         }
         return minDistance;
     }
+
     public static double euclideanDistance(Position p1, Position p2) {
         return Math.sqrt(Math.pow(p1.getY() - p2.getY(), 2) + Math.pow(p1.getX() - p2.getX(), 2));
     }
@@ -32,46 +34,66 @@ public class DistanceService {
     }
 
     public static void setRoomMap(RoomMap roomMap) {
-        Graph<Position, UndirectedWeightedEdge> graph= new DefaultUndirectedWeightedGraph<>(UndirectedWeightedEdge.class);
-        Set<Position> verticesPositions= roomMap.getVisualDictionary().keySet();
+        Graph<Position, UndirectedWeightedEdge> graph = new DefaultUndirectedWeightedGraph<>(UndirectedWeightedEdge.class);
+        Set<Position> verticesPositions = roomMap.getVisualDictionary().keySet();
         for (Position vertexPosition : verticesPositions) {
             graph.addVertex(vertexPosition);
+            pathsGraph.addVertex(vertexPosition);
         }
         for (Position vertexPosition : verticesPositions) {
-           addEdges(graph,vertexPosition,roomMap.getVisualDictionary());
+            addEdges(graph, vertexPosition, roomMap.getVisualDictionary());
         }
         dijkstraShortestPath = new DijkstraShortestPath(graph);
     }
 
 
     private static void addEdges(Graph<Position, UndirectedWeightedEdge> graph, Position vertexPosition, HashMap<Position, HashSet<Position>> verticesPositions) {
-        Position moveTo=new Position(vertexPosition.getY()-1,vertexPosition.getX());
-        addEdgeIfValid(graph,vertexPosition,verticesPositions,moveTo);
+        Position moveTo = new Position(vertexPosition.getY() - 1, vertexPosition.getX());
+        addEdgeIfValid(graph, vertexPosition, verticesPositions, moveTo);
 
-        moveTo=new Position(vertexPosition.getY()+1,vertexPosition.getX());
-        addEdgeIfValid(graph,vertexPosition,verticesPositions,moveTo);
+        moveTo = new Position(vertexPosition.getY() + 1, vertexPosition.getX());
+        addEdgeIfValid(graph, vertexPosition, verticesPositions, moveTo);
 
-        moveTo=new Position(vertexPosition.getY(),vertexPosition.getX()-1);
-        addEdgeIfValid(graph,vertexPosition,verticesPositions,moveTo);
+        moveTo = new Position(vertexPosition.getY(), vertexPosition.getX() - 1);
+        addEdgeIfValid(graph, vertexPosition, verticesPositions, moveTo);
 
-        moveTo=new Position(vertexPosition.getY(),vertexPosition.getX()+1);
-        addEdgeIfValid(graph,vertexPosition,verticesPositions,moveTo);
+        moveTo = new Position(vertexPosition.getY(), vertexPosition.getX() + 1);
+        addEdgeIfValid(graph, vertexPosition, verticesPositions, moveTo);
 
     }
 
-    private static void addEdgeIfValid(Graph<Position, UndirectedWeightedEdge> graph, Position vertexPosition, HashMap<Position, HashSet<Position>> verticesPositions,Position moveTo){
-        if(verticesPositions.containsKey(moveTo)){
+    private static void addEdgeIfValid(Graph<Position, UndirectedWeightedEdge> graph, Position vertexPosition, HashMap<Position, HashSet<Position>> verticesPositions, Position moveTo) {
+        if (verticesPositions.containsKey(moveTo)) {
             UndirectedWeightedEdge edge = graph.addEdge(vertexPosition, moveTo);
-            if(edge!=null)
+            if (edge != null)
                 graph.setEdgeWeight(edge, 1);
         }
     }
 
-    public static double minPathWeight(Position source, Position sink){
-        return dijkstraShortestPath.getPathWeight(source,sink);
+    public static double minPathWeight(Position source, Position sink) {
+        return dijkstraShortestPath.getPathWeight(source, sink);
     }
 
-    public static ShortestPathAlgorithm.SingleSourcePaths<Position, UndirectedWeightedEdge> getPositionPaths(Position position){
+    public static ShortestPathAlgorithm.SingleSourcePaths<Position, UndirectedWeightedEdge> getPositionPaths(Position position) {
         return dijkstraShortestPath.getPaths(position);
+    }
+
+    public static double getWeight(Position current, Position other) {
+        UndirectedWeightedEdge edge = pathsGraph.getEdge(current, other);
+        if (edge == null) {
+            addEdgesToPathsGraph(getPositionPaths(current),current);
+            edge = pathsGraph.getEdge(current, other);
+        }
+        return pathsGraph.getEdgeWeight(edge);
+    }
+
+    private static void addEdgesToPathsGraph(ShortestPathAlgorithm.SingleSourcePaths<Position, UndirectedWeightedEdge> positionPaths, Position position) {
+        UndirectedWeightedEdge edge;
+        for (Position vertex : pathsGraph.vertexSet()) {
+            if(!pathsGraph.containsEdge(position,vertex)){
+                edge=pathsGraph.addEdge(position,vertex);
+                pathsGraph.setEdgeWeight(edge,positionPaths.getWeight(vertex));
+            }
+        }
     }
 }
