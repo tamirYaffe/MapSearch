3abae35ce54b0b74f72e753f8e357cb4299064a1
tamiryaffe <tamiryaffe144@gmail.com>
2020-12-21 20:31:21 +0200

    limited radius for los and open new branch

--- src/Search/VisualLineOfSightAdapter.java
+++ src/Search/VisualLineOfSightAdapter.java
@@ -1,6 +1,7 @@
 package Search;
 
 import rlforj.los.*;
+import java.lang.Math;
 
 public class VisualLineOfSightAdapter {
     private ILosAlgorithm los;
@@ -10,7 +11,10 @@ public class VisualLineOfSightAdapter {
     }
 
     public boolean existsLineOfSight(ILosBoard b, int startX, int startY, int x1, int y1) {
-        return los.existsLineOfSight(b, startX, startY, x1, y1, true);
+//        return los.existsLineOfSight(b, startX, startY, x1, y1, true);
+        boolean los_exists = los.existsLineOfSight(b, startX, startY, x1, y1, true);
+        boolean in_radius = Math.sqrt(Math.pow(startX - x1, 2) + Math.pow(startY - y1, 2)) <= 20;
+        return los_exists && in_radius;
     }
 
     public String getLosName() {
